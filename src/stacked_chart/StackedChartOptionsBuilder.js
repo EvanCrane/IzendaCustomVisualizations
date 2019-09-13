1e80ecfc0306be99ec689d193ca8453e9ca265f5
EvanCrane <evanf.crane@gmail.com>
2019-09-13 15:32:19 -0400

    Added stacking all columns into 1 by default for each series

--- src/stacked_chart/StackedChartOptionsBuilder.js
+++ src/stacked_chart/StackedChartOptionsBuilder.js
@@ -12,8 +12,24 @@ export default class StackedChartOptionsBuilder extends CombinationChartOptionsB
         // Get chart options from CombinationChartOptionsBuilder
         let chartOptions = super.buildOptionsByType(visualType, userOptions, dataParser);
 
+        // Iterate through each series
+        chartOptions.series.forEach(serie => {
+            if (serie.type === 'column'){
+                serie.stack = 1;
+            }
+        });
+
         // Extend HighChart chart options
         $.extend(true, chartOptions, {
+            tooltip: {
+                shared: true,
+                crosshairs: true
+            },
+            plotOptions: {
+                column: {
+                    stacking: 'percentage'
+                }
+            }
         });
 
         return chartOptions;
