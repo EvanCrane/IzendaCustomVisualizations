bcc0644cbff975fba68f366a8d3e2a311de7a346
EvanCrane <evanf.crane@gmail.com>
2019-09-18 11:02:09 -0400

    Removing unneeded custom visualizations

--- src/3dcommon.js
+++
@@ -1,6 +0,0 @@
-import { getClass } from 'IzendaSynergy';
-import highchart3d from 'highcharts/highcharts-3d';
-
-const { VisualizationLibrary } = getClass('HighchartVizEngine');
-
-highchart3d(VisualizationLibrary);
